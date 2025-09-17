859babaecc6533126ffabd9a71829d9ad9509219
smithamaris <smith.amaris@gmail.com>
2025-09-17 17:24:05 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+//1. Adding New Tasks
+// Create input fields for the task name, category, deadline, and an initial status (e.g., “In Progress”).
+// Include an “Add Task” button that will add the task to the task list.
+// Each task should be stored as an object with properties such as task name, category, deadline, and status.
+// Add the task object to an array that holds all tasks.
+
